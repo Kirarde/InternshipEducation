f4d090581b12468b1548345f258909618d0e5f09
Kirarde <emrecansaltik@gmail.com>
2023-02-17 16:49:04 +0300

    17.02.2023 16.48

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,20 +2,24 @@
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-//ProductTest();
-CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-foreach (var category in categoryManager.GetAll())
-{
-    Console.WriteLine(category.CategoryName);
-}
+ProductTest();
+//CategoryTest();
 
 static void ProductTest()
 {
     ProductManager productManager = new ProductManager(new EfProductDal());
 
-    foreach (var product in productManager.GetByUnitPrice(40, 100))
+    foreach (var product in productManager.GetProductDetails())
     {
-        Console.WriteLine(product.ProductName);
+        Console.WriteLine(product.ProductName + "/" + product.CategoryName);
     }
 }
 
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
+}
